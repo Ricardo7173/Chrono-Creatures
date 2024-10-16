8217fff7dcb7e9d44d15c7d4d3f74244c6e5c828
mictlangelo10 <miguehdzss29@gmail.com>
2024-10-16 11:30:26 -0600

    Se crea el menú principal y funciona al darle clic en play

---
+++ Assets/Scripts/Menú/MenuController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Asegúrate de que esta línea esté presente
+
+
+public class MenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Nivel 1"); 
+    }
+}
+
+
+
